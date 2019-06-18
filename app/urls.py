c6ba5a476d00bcaf32845e27a70fa4aade997d76
Stone.F.Yang (tw mis.twtp01.Newegg) 61589 <sy43@buyabs.corp>
2019-06-18 10:48:32 +0800

    update url

--- app/urls.py
+++ app/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('users/<int:id>', views.UserAPIView.as_view()),
-    path('users', views.UserListAPIView.as_view()),
-    path('depts/<int:id>', views.DeptAPIView.as_view()),
-    path('depts', views.DeptListAPIView.as_view()),
-    path('projs/<int:id>', views.ProjAPIView.as_view()),
-    path('projs', views.ProjListAPIView.as_view()),    
+    path('user/<int:id>', views.UserAPIView.as_view()),
+    path('user', views.UserListAPIView.as_view()),
+    path('dept/<int:id>', views.DeptAPIView.as_view()),
+    path('dept', views.DeptListAPIView.as_view()),
+    path('proj/<int:id>', views.ProjAPIView.as_view()),
+    path('proj', views.ProjListAPIView.as_view()),    
     path('init/<str:type>', views.InitAPIView.as_view()),  
 ]
